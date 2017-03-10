1e002f1c32573693885330405f02be0ffa261470
Silent_DXX <silent_dxx@163.com>
2017-03-10 11:04:58 +0800

    Add all project files

---
+++ main.js
@@ -0,0 +1,36 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () {
+  //mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ transparent: true, frame: false })
+  //mainWindow = new BrowserWindow({frame: false, fullscreen: true})
+
+  mainWindow.on('close', function () { win = null })
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  shell.beep()
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+
